81d469bb6b32ab72788ec926f64e8dee9ef6dba3
EladB1 <eb86@njit.edu>
2022-07-21 15:11:52 -0400

    Initial commit

---
+++ src/test/java/com/piedpiper/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piedpiper.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
